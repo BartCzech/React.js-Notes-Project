73092cc590b34ebc3d039ee2a2bb34a79e85051f
“Imię <nazwisko@imie.com>
2023-04-08 16:35:51 +0200

    Added CRUD methods to api.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('./db/mongoose');
 // routes
 const apiRouter = require('./routes/api');
 
-app.use('/', apiRouter);
+app.use('/api/', apiRouter);
 
 app.listen(config.port, function () {
   console.log("Listening on port " + config.port);
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,19 @@ const router = express.Router();
 
 const noteActions = require('../actions/api/notes');
 
-router.get("/", noteActions.saveNote);
+// Creating notes
+router.post("/notes/", noteActions.saveNote);
+
+// Reading notes
+router.get("/notes", noteActions.getAllNotes);
+// Reading 1 note
+router.get("/notes/{id}", noteActions.getNote);
+
+// Updating notes
+router.put("/notes/", noteActions.saveNote);
+
+// Deleting notes
+router.delete("/notes/", noteActions.saveNote);
+
 
 module.exports = router;
