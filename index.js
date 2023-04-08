bd8c8306fe86084f34d563cc29b7ff492f5814c0
“Imię <nazwisko@imie.com>
2023-04-08 13:52:37 +0200

    Server config, port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const config = require('./config');
 
 app.get("/", function (req, res) {
   res.send("Server running");
 });
 
-app.listen(3000, function () {
-  console.log("Listening on port 3000");
+app.listen(config.port, function () {
+  console.log("Listening on port " + config.port);
 });
