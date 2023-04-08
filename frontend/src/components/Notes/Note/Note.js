2e99c19de15cb065d3d1b5c58f0fe5791740204a
“Imię <nazwisko@imie.com>
2023-04-09 00:09:50 +0200

    Basic frontend (Notes.js and Note.js) added.

---
+++ frontend/src/components/Notes/Note/Note.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note(props) {
+  return (
+    <div className="note">
+      <p>{props.title}</p>
+      <div className="description">{props.body}</div>
+      <button>Edit</button>
+      <button className="delete">Delete</button>
+    </div>
+  );
+}
+
+export default Note;
--- frontend/src/components/Notes/Notes.js
+++ frontend/src/components/Notes/Notes.js
@@ -1,13 +1,35 @@
 import React from "react";
+import "./Notes.css";
+import Note from "./Note/Note";
 
 class Notes extends React.Component {
-    render() {
-        return (
-            <div>
-                my notes
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+
+    this.notes = [
+      {
+        id: "2323",
+        title: "Take a shower",
+        body: "Remember to use the special shampoo.",
+      },
+      {
+        id: "2324",
+        title: "Alışveriş yap",
+        body: "Süt, tereyağı, şerbet.",
+      },
+    ];
+  }
+  render() {
+    return (
+      <div>
+        <p>My notes</p>
+
+        {this.notes.map((note) => {
+          return <Note title={note.title} body={note.body} id={note.id} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default Notes;
