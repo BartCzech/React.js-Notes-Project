daca8786596748a21b8730376b7117ef0a85ae4c
“Imię <nazwisko@imie.com>
2023-04-10 12:52:27 +0200

    NotificationManager: notifs working

--- frontend/src/components/Notes/Notes.js
+++ frontend/src/components/Notes/Notes.js
@@ -5,6 +5,8 @@ import NewNote from "./NewNote/NewNote";
 import Modal from "react-modal";
 import EditNote from "../EditNote/EditNote";
 import axios from '../../axios';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
 
 class Notes extends React.Component {
   constructor(props) {
@@ -45,7 +47,7 @@ class Notes extends React.Component {
       notes.push(newNote);
       this.setState({ notes });
     } catch (err) {
-      console.log(err.response.data.message);
+      NotificationManager.error(err.response.data.message); 
     };
   }
 
@@ -75,6 +77,8 @@ class Notes extends React.Component {
   render() {
     return (
       <div>
+        <NotificationContainer />
+
         <p>My notes</p>
 
         <NewNote onAdd={(note) => this.addNote(note)} />
